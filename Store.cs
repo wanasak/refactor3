6946f6eb2ee1d029256672db64711df6e6919059
Wanasak <u510610433@gmail.com>
2017-06-14 23:50:18 +0700

    fix bad code 2

--- Store.cs
+++ Store.cs
@@ -14,10 +14,20 @@ namespace refractor3_demo
         {
             foreach (var product in _products)
             {
+                int numberOfProduct = product.GetQty();
+                string productName = product.GetName();
+                double cost = product.GetTotalCost();
+                double costWithDiscount = product.GetTotalCost() / product.GetQty();
+                double costWithoutDiscount = product.GetPrice() + product.GetShippingCost();
+
+                System.Console.WriteLine("Total cost for {0} is {1:N2}", productName, cost);
+                System.Console.WriteLine("Cost per product {0:N2}", costWithDiscount);
+                System.Console.WriteLine("Savings per product {0:N2}", costWithoutDiscount - costWithDiscount);
+
                 // BAD CODE
-                System.Console.WriteLine("Total cost for {0} is {1:N2}", product.GetName(), product.GetTotalCost());
-                System.Console.WriteLine("Cost per product {0:N2}", product.GetTotalCost() / product.GetQty());
-                System.Console.WriteLine("Savings per product {0:N2}", (product.GetPrice() + product.GetShippingCost()) - (product.GetTotalCost() / product.GetQty()));
+                // System.Console.WriteLine("Total cost for {0} is {1:N2}", product.GetName(), product.GetTotalCost());
+                // System.Console.WriteLine("Cost per product {0:N2}", product.GetTotalCost() / product.GetQty());
+                // System.Console.WriteLine("Savings per product {0:N2}", (product.GetPrice() + product.GetShippingCost()) - (product.GetTotalCost() / product.GetQty()));
                 // END BAD CODE
             }
         }
